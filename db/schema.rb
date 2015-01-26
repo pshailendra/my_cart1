095d80b3a9a4acf7493934a22b5626913bdf1121
Shailendra <shailedrammec@gmail.com>
2015-01-26 16:29:26 +0530

    Cart Added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150125193140) do
+ActiveRecord::Schema.define(version: 20150126105703) do
+
+  create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "friendly_id_slugs", force: true do |t|
     t.string   "slug",                      null: false
